348ead47cafeb5162093db4d7ce8132dd3f36710
Carlos Castillo Alarcón <castillo.st@gmail.com>
2017-03-05 10:05:50 +0100

    Modifico validación de tipos en pokemons
    Las validaciones de número máximo y mínimo de pokémons para que en lugar de ser custom sean con validatesLengthOf

--- server/models/pokemon.js
+++ server/models/pokemon.js
@@ -1,24 +1,12 @@
 'use strict';
 
 module.exports = function(Pokemon) {
-  Pokemon.validate('maximoNumeroTipos', maximoTiposValidador, {
-    message: 'Como máximo se pueden indicar dos tipos de pokemon.'
-  });
-
-  Pokemon.validate('minimoNumeroTipos', minimoTiposValidador, {
-    message: 'Debe indicarse al menos un tipo de pokemon.'
-  });
-
-  function maximoTiposValidador(err) {
-    if (this.tipoIds &&  this.tipoIds.length > 2) {
-      err();
-    }
-  }
-
-  function minimoTiposValidador(err) {
-    if (!this.tipoIds || this.tipoIds.length === 0) {
-      err();
-    }
-  }
+  Pokemon.validatesLengthOf('tipoIds', {
+    min: 1,
+    max: 2,
+    message: {
+      min: 'Como máximo se pueden indicar dos tipos de pokemon.',
+      max: 'Debe indicarse al menos un tipo de pokemon.',
+    }});
 };
 
