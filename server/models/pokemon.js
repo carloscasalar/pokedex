640adade4e4d70a848c0c56ec29067e4285ec246
Carlos Castillo Alarcón <castillo.st@gmail.com>
2017-03-05 10:21:45 +0100

    Agrego validación del nombre del pokemon

--- server/models/pokemon.js
+++ server/models/pokemon.js
@@ -8,5 +8,11 @@ module.exports = function(Pokemon) {
       min: 'Como máximo se pueden indicar dos tipos de pokemon.',
       max: 'Debe indicarse al menos un tipo de pokemon.',
     }});
+
+  Pokemon.validatesFormatOf('nombre', {
+    with: /^\w{4,24}$/,
+    allowNull: false,
+    message: 'El nombre debe ser una única palabra entre 4 y 24 caracteres.',
+  });
 };
 
