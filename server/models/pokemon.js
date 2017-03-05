4d31e60babbca01287f5930fe79b7bf3c6842e3d
Carlos Castillo Alarcón <castillo.st@gmail.com>
2017-03-05 10:25:56 +0100

    Agrego validación de descripción
    Corrijo tb las de descripción, que estaban al revés

--- server/models/pokemon.js
+++ server/models/pokemon.js
@@ -5,8 +5,8 @@ module.exports = function(Pokemon) {
     min: 1,
     max: 2,
     message: {
-      min: 'Como máximo se pueden indicar dos tipos de pokemon.',
-      max: 'Debe indicarse al menos un tipo de pokemon.',
+      min: 'Debe indicarse al menos un tipo de pokemon.',
+      max: 'Como máximo se pueden indicar dos tipos de pokemon.',
     }});
 
   Pokemon.validatesFormatOf('nombre', {
@@ -14,5 +14,11 @@ module.exports = function(Pokemon) {
     allowNull: false,
     message: 'El nombre debe ser una única palabra entre 4 y 24 caracteres.',
   });
+
+  Pokemon.validatesLengthOf('descripcion', {
+    min: 30,
+    message: {
+      min: 'La descripción debe ser de al menos 30 caracteres',
+    }});
 };
 
