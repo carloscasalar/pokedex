c8bca8c60494dc684da8f7fd92ba88046196e436
Carlos Castillo Alarcón <castillo.st@gmail.com>
2017-03-05 01:32:25 +0100

    Añado validadores tipo a pokemons
    Añado al modelo de pokemos validadores para regir el mínimo y el máximo número de tipos de pokemon que debe tener un pokemon

--- server/models/pokemon.js
+++ server/models/pokemon.js
@@ -1,5 +1,24 @@
 'use strict';
 
 module.exports = function(Pokemon) {
+  Pokemon.validate('maximoNumeroTipos', maximoTiposValidador, {
+    message: 'Como máximo se pueden indicar dos tipos de pokemon.'
+  });
 
+  Pokemon.validate('minimoNumeroTipos', minimoTiposValidador, {
+    message: 'Debe indicarse al menos un tipo de pokemon.'
+  });
+
+  function maximoTiposValidador(err) {
+    if (this.tipoIds &&  this.tipoIds.length > 2) {
+      err();
+    }
+  }
+
+  function minimoTiposValidador(err) {
+    if (!this.tipoIds || this.tipoIds.length === 0) {
+      err();
+    }
+  }
 };
+
